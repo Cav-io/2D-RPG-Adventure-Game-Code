5a164841f824b25e388a6f904e4457a339464809
Cavio Hossain <92102170+Cav-io@users.noreply.github.com>
2022-11-20 15:51:46 +0000

    Added comments to init.js and creates a new instance for the game

--- init.js
+++ init.js
@@ -1,7 +1,6 @@
-(function () {
-  const game = new Game({
-    element: document.querySelector(".game")
+const game = new Game({ //Creates a new instance of the game
+    element: document.querySelector(".game") 
+    //the game class is declared as the element where the game will run
   });
 
-  game.init();
-})();
+game.init(); //this method will start the game instance
