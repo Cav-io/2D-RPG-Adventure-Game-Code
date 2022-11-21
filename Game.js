ea5a442f3ccb404ccf629697613330ab93180d3e
Cavio Hossain <92102170+Cav-io@users.noreply.github.com>
2022-11-21 13:53:21 +0000

    I added drawMap function and added three layers

--- Game.js
+++ Game.js
@@ -9,9 +9,32 @@ class Game {
     //Will give us access to different drawing methods
   }
 
+  drawMap(imageInstance, posX, posY) { //function that draws images on the canvas
+    //The parameters is the image object, x and y position that will be 
+    
+    imageInstance.onload = () => {
+      this.context.drawImage(imageInstance,posX, posY)
+    }
+  } 
+  
   //The init method will start the game
   init() {
-    console.log("test2", this); //testing if it initiates
+    
+    //Lower layer
+    const lowerLayer = new Image(); //creates a a new image 
+    lowerLayer.src = "/Maps/starting house/lower layer.png"; 
+    //The source is the attribute of the instance  
+    this.drawMap(lowerLayer,0,0); //Calls the method for drawing the image
+    
+    //Collision Layer
+    const collisionLayer = new Image();
+    collisionLayer.src = "/Maps/starting house/collision layer.png";
+    this.drawMap(collisionLayer,0,0);
+    
+    //Upper Layer
+    const upperLayer = new Image();
+    upperLayer.src = "/Maps/starting house/upper layer.png";
+    this.drawMap(upperLayer,0,0);
   }
   
 }
