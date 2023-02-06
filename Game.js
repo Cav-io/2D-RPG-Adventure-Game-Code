113e4b0bd9807a0ec1dd22cd0dda8bbfb51b36e2
Cavio Hossain <92102170+Cav-io@users.noreply.github.com>
2023-02-06 19:43:14 +0000

    Added Comments to game.js and map class.js

--- Game.js
+++ Game.js
@@ -17,18 +17,27 @@ class Game {
       //Calls a function before going repainting the next frame
       requestAnimationFrame(() => { 
         
-        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height); //Clears the canvas
+        //Clears the canvas
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height); 
+
+        //Creates a list for entities
         const entities = Object.values(this.map.entities);
+        //Adds player as an entity
         entities.push(player);
-        
+
+        //Update each entity in the map
         entities.forEach(entity => {
           entity.update({
+            //Passed on for any keyInput for movement
             direction: this.keyInput.direction,
+            //Passed on for any keyInput for speed toggle
             speedBoost: this.keyInput.speedBoost,
+            //Passed on to check for any collision tiles
             map: this.map
           });
         });
-                                                 
+
+        //Draw lower and collision layers
         this.map.drawLower(this.context, player);
         this.map.drawCollision(this.context, player);
 
@@ -36,10 +45,12 @@ class Game {
         Object.values(entities).forEach(entity => {
           entity.sprite.drawObj(this.context, player);
         })
-        
 
+        //Draw Upper Layer
         this.map.drawUpper(this.context, player);
-        
+
+
+        //Check for player exiting or entering a new map
         Object.entries(this.map.exits).forEach(([key, exit]) => {
           if (player.x / 16 === exit.x && player.y / 16 === exit.y) {
             this.map = window.mapDict[this.map.update(key, exit)];
@@ -47,22 +58,32 @@ class Game {
           }
         });
         
-        gameLoop(); //Re-iterates the function
+        //Re-iterates the function infinitely
+        gameLoop();
       })
     }
-    gameLoop(); //Initiates the game loop
+    //Initiates the game loop
+    gameLoop(); 
   }
   
-  //The init method will start the game
+  //The init method will Initiate the game
   init() {
+    //Initiates Player as a global object
     window.player = new Player({
       name: "MaskedNinja",
       x: 4, y: 4
-    })
+    });
+    
+    //Initiates the starting map 
     this.map = mapDict.StartingHouse
+    //Gets map coordinates to check for collisions
     this.map.fetchCoordinates()
+
+    //Intialises a keyInput instance
     this.keyInput = new keyInput()
     this.keyInput.init()
+
+    //Intialises the game loop
     this.Loop();
   }
 }
